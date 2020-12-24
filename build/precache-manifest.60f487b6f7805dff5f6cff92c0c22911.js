661051009ac222a4ca3e197239ff750bf51a7147
abhijeet <letsmailabhijeet@gmail.com>
2020-12-24 23:10:34 +0530

    Posting the Build of Basic shop Details Entry Application

---
+++ build/precache-manifest.60f487b6f7805dff5f6cff92c0c22911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f630354f959acf64d1cce18e350f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "470399fe49a864feef44",
+    "url": "/static/css/main.52b06a7b.chunk.css"
+  },
+  {
+    "revision": "18446aaa9ec517549dfc",
+    "url": "/static/js/2.26077606.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26077606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470399fe49a864feef44",
+    "url": "/static/js/main.094f400d.chunk.js"
+  },
+  {
+    "revision": "1877e69cc92e77fffa19",
+    "url": "/static/js/runtime-main.147a4b9c.js"
+  }
+]);
